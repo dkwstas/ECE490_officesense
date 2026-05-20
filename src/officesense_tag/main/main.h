3977e8d17b2db24afbcc0ba14b8e15612f9b5790
Kostas Drakontidis <gedra100sh@gmail.com>
2026-05-20 17:44:46 +0300

    Tag config management code

---
+++ src/officesense_tag/main/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "host/ble_hs.h"
+
+#define DEFAULT_UUID "12345678-9abc-def0-1234-56789abcdef0"
+#define DEFAULT_NAME "X6TAG"
+#define DEFAULT_ADV_INTERVAL 0x80
+
+typedef struct
+{
+    ble_uuid128_t uuid;
+    char uuid_str[37];
+    char name[32];
+    uint32_t adv_interval;
+} config_t;
+
+#endif
