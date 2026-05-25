93c65a948b3973977bcb8d0cf93d1a23ee5afc6b
Kostas Drakontidis <kdrakontidis@uth.gr>
2026-05-25 05:54:17 +0300

    added routes

--- src/officesense_pi/src/index.ts
+++ src/officesense_pi/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import lookupRoutes from "./api/lookup/lookup.routes.js";
+import livedataRoutes from "./api/livedata/livadata.routes.js"
 import config from "./config/config.js";
 import { bootstrap } from "./core/bootstrap.js";
 
 (async () => {
     const app = express();
 
-    app.use("/", lookupRoutes);
+    app.use("/lookup", lookupRoutes);
+    app.use("/livedata", livedataRoutes);
 
     app.listen(config.api.port, config.api.address, () => {
         console.log(`Server listening on ${config.api.address}:${config.api.port}`);
